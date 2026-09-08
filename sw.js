93710f6f5635ec69378a32ead6b0d355903b465f
Longevity <fredygun123@gmail.com>
2026-09-09 00:32:47 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = "workout-log-v1";
+const APP_SHELL = ["./", "./index.html", "./manifest.json", "./icons/icon-192.png", "./icons/icon-512.png"];
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
+});
+self.addEventListener("activate", event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener("fetch", event => {
+  const req = event.request;
+  const url = new URL(req.url);
+  if (req.method !== "GET" || url.origin !== self.location.origin) return;
+  event.respondWith(caches.match(req).then(cached => cached || fetch(req).then(res => {
+    if (res.ok) { const clone = res.clone(); caches.open(CACHE_NAME).then(c => c.put(req, clone)); }
+    return res;
+  }).catch(() => caches.match("./index.html"))));
+});
