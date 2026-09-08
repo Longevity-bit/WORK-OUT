664ed8b5b490f5f3ad2ac74bb5091661e4a8974b
Longevity <fredygun123@gmail.com>
2026-09-09 02:06:14 +0700

    update cache name v2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "workout-log-v1";
+const CACHE_NAME = "workout-log-v2";
 const APP_SHELL = ["./", "./index.html", "./manifest.json", "./icons/icon-192.png", "./icons/icon-512.png"];
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
